9a4fe838939f257f5a2cc3bf677df6bc5d90904d
ostmail@163.com <ostmail@163.com>
2018-06-14 20:35:48 +0800

    use websocekt

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello():
-    return 'hello!! world!!!!!'
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+
+@socketio.on('client_event')
+def client_msg(msg):
+    emit('server_response', {'data': msg['data']})
+
+
+@socketio.on('connect_event')
+def connected_msg(msg):
+    emit('server_response', {'data': msg['data']})
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0')
