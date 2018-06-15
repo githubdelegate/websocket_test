dbd94be693f7fd7c99d8e9dcd3e7f5215c4cd2b3
ostmail@163.com <ostmail@163.com>
2018-06-15 18:43:50 +0800

    websocket run

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit, send
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -14,12 +15,36 @@ def index():
 
 @socketio.on('client_event')
 def client_msg(msg):
+    print('msg')
     emit('server_response', {'data': msg['data']})
-
 
 @socketio.on('connect_event')
 def connected_msg(msg):
+    print('connect')
     emit('server_response', {'data': msg['data']})
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    print('connect llllla')
+    # emit('connect', {'data': 'Connected'})
+    send('connect')
+    time.sleep(3)
+    emit('connect', '123')
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
+@socketio.on_error_default
+def default_error_handler(e):
+    print(request.event["message"]) # "my error event"
+    print(request.event["args"])
+
+@socketio.on_error()
+def error_handler(e):
+    print(e)
 
 
 if __name__ == '__main__':
---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import *
+from fabric.operations import sudo
+
+env.hosts = ['45.114.127.141']            # 指定 hosts 远程主机
+# env.key_filename = '/path/to/id_rsa'     # 指定你的私钥文件
+env.user = 'root'
+env.password = '515298403849'
+
+def deploy():
+    project_folder = '/root/websocket_test'
+    with cd(project_folder):
+        run('pwd')
+        run('git pull')
+        run('.venv/bin/gunicorn stop')
+        run('.venv/bin/gunicorn --reload -b 127.0.0.1:8000 app:app')
+
+
+
+
