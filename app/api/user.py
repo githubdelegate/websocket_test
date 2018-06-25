26e14bb72e1f78628a69ca0e46a5739f89f457a6
ostmail@163.com <ostmail@163.com>
2018-06-25 16:19:38 +0800

    some change

--- app/api/user.py
+++ app/api/user.py
@@ -5,5 +5,5 @@ from flask import jsonify
 # 获取用户的好友列表
 @api.route('/user/friends/')
 def friends():
-    d = {'error': 0, 'result': ['zy', 'nj']}
+    d = {'error': 0, 'result': ['zy', 'nj', '哈哈']}
     return jsonify(d)
